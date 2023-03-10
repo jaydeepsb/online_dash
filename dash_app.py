d6e71b83dccec4d33d890714981b37b8095e0003
Jaydeep <8065990+jaydeepsb@users.noreply.github.com>
2023-03-10 08:13:46 +0100

    Add files via upload

---
+++ dash_app.py
@@ -0,0 +1,46 @@
+import time
+import numpy as np
+
+import dash
+from dash import Dash, dcc, html, Input, Output, ctx, State
+from dash import dash_table as dt
+import dash_bootstrap_components as dbc
+from dash.exceptions import PreventUpdate
+
+app = Dash(__name__)
+
+
+# Define the layout of the app
+app.layout = html.Div([
+    html.H1('Type Alphabets'),
+    dcc.Input(id='input', type='text', value=''),
+    html.Button('+', id='button-inc', n_clicks=0),
+    html.Button('-', id='button-dec', n_clicks=0),
+    html.Div(id='output', style={'fontSize': 80}),
+])
+
+
+# Define the callback functions
+@app.callback(
+    Output('output', 'children'),
+    Input('input', 'value'),
+    Input('button-inc', 'n_clicks'),
+    Input('button-dec', 'n_clicks'),
+)
+def update_output(input_value, n_clicks_inc, n_clicks_dec):
+    # Determine the font size based on the number of clicks
+    font_size = 24 + 2 * (n_clicks_inc - n_clicks_dec)
+    font_size = max(10, min(font_size, 36))
+
+    # # Determine the text color based on the color that was clicked
+    # ctx = dash.callback_context
+    # color_id = ctx.triggered[0]['prop_id'].split('.')[0]
+    # color = colors.get(color_id, 'black')
+
+    # Return the updated output
+    return html.Div(input_value, style={'fontSize': font_size})
+
+if __name__ == "__main__":
+    #141.48.22.21
+    #app.run_server(debug=False, port=8050, host='0.0.0.0')
+    app.run_server(debug=True, port=8050, host='127.0.0.1')
